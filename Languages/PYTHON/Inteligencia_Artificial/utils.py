31351dc8f41a9738fa11258fb17be339e13a370f
Zseiru15 <co4786696@gmail.com>
2026-08-29 15:56:50 -0500

    creacion de condicionales, funciones, y archivo utlis para funciones organizadas en un archivo externo para importar

---
+++ Languages/PYTHON/Inteligencia_Artificial/utils.py
@@ -0,0 +1,13 @@
+#
+def porcentaje (num1, num2):
+    """
+    Dado 2 valores el primero porcentaje y el segundo valor.
+    Calcula el porcentaje especificado.
+
+    argumento
+    num1: es el porcentaje de 0 - 100
+    num2: es el valor
+    """
+    p = num2 * num1/100
+
+    return p
